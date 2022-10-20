b895488884a5c55569952ad05ff8a9d38f448c89
Nastia <anastasiia.petrovych@ucu.edu.ua>
2022-10-20 15:32:32 +0300

    Update Chamomile.java

--- src/main/java/flower/store/Chamomile.java
+++ src/main/java/flower/store/Chamomile.java
@@ -3,7 +3,7 @@ import lombok.Getter;
 
 public class Chamomile extends Flower {
     @Getter
-    public String name = "chamomile";
+    private String name = "chamomile";
     @Override
     public  String toString() {
         return "The charming chamomile is found";
