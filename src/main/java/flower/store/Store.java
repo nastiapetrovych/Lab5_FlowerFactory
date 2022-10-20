b97d410b008048b5dcb253715543a16a3bc024fa
Nastia <anastasiia.petrovych@ucu.edu.ua>
2022-10-20 15:33:47 +0300

    Update Store.java

--- src/main/java/flower/store/Store.java
+++ src/main/java/flower/store/Store.java
@@ -9,8 +9,9 @@ public class Store {
     Map<Object, Integer> catalog = new HashMap<Object, Integer>();
 
     public void addFlower(Flower flower) {
-        if (catalog.containsKey(flower.getFlowerType())){
-            catalog.put((flower.getFlowerType()), catalog.get(flower.getFlowerType()) + 1);
+        if (catalog.containsKey(flower.getFlowerType())) {
+            catalog.put((flower.getFlowerType()), 
+                        catalog.get(flower.getFlowerType()) + 1);
         }
         else {
             catalog.put((flower.getFlowerType()), 1);
@@ -18,7 +19,8 @@ public class Store {
     }
     public static String search(Flower flower) {
         if ((catalog.get(flower.getFlowerType()) > 0)) {
-            catalog.put((flower.getFlowerType()), catalog.get(flower.getFlowerType()) - 1);
+            catalog.put((flower.getFlowerType()), 
+                        catalog.get(flower.getFlowerType()) - 1);
              return flower.toString();
         }
         else {
